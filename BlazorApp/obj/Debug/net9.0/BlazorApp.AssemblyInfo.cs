391b21a55d8f76c17bc631f6dc86c1b71c2751f6
The-Immortal-Wanderer <madham123098@gmail.com>
2025-04-06 12:10:13 +0500

    Admin/User views set, hrefs set. ToDo: 1.make admin/user pages be different, 2.delete user and delete book functionality needs implementation

--- BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0ad79115359ba0bc7051eef65ee3d54c745ee6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33d198e803dca71475b1490c41049730cd727cd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
