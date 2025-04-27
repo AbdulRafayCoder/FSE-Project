27731f36bf00a3b90266583612be0587eb1804dd
The-Immortal-Wanderer <madham123098@gmail.com>
2025-04-27 13:33:35 +0500

    finalized frontend | Project completed.

--- BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0872ed03700f303a740571159db5ecdea46f8b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d08b27cb9ba23f763df358f21dc796e18f39e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
